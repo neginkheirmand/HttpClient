71f64671f78bf61239ae0111c422133147ea9037
9831023 <9831023@ceit.local>
2020-05-03 23:26:23 +0430

    created project

---
+++ GUI/Main.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("Insomnia");
+        JMenuBar menuBarOfInsomnia = new JMenuBar();
+        //---------------
+        JMenu Application = new JMenu();
+        JMenu Application = new JMenu();
+        JMenu Application = new JMenu();
+        JMenu Application = new JMenu();
+        JMenu Application = new JMenu();
+        //--------------
+        JMenuItem Preferences = new JMenuItem("Preferences");
+        JMenuItem Changelog = new JMenuItem();
+        JMenuItem HideInsomnia = new JMenuItem("Changelog");
+        JMenuItem HideOthers = new JMenuItem("HideOthers");
+        JMenuItem Quit = new JMenuItem("Quit");
+
+    }
+}
