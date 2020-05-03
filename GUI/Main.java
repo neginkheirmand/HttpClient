5252db9eb1e51e539d8e784d52406025bc4e867e
9831023 <9831023@ceit.local>
2020-05-04 00:49:47 +0430

    preparing the menu

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,23 +1,176 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class Main {
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame("Insomnia");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+
         JMenuBar menuBarOfInsomnia = new JMenuBar();
         //---------------
-        JMenu Application = new JMenu();
-        JMenu Application = new JMenu();
-        JMenu Application = new JMenu();
-        JMenu Application = new JMenu();
-        JMenu Application = new JMenu();
+        JMenu application = new JMenu("Application");
+        application.setMnemonic(KeyEvent.VK_A);
+        JMenu edit = new JMenu("Edit");
+        edit.setMnemonic(KeyEvent.VK_E);
+        JMenu view = new JMenu("View");
+        view.setMnemonic(KeyEvent.VK_V);
+        JMenu window = new JMenu("Window");
+        window.setMnemonic(KeyEvent.VK_W);
+        JMenu tools = new JMenu("Tools");
+        tools.setMnemonic(KeyEvent.VK_T);
+        JMenu help = new JMenu("Help");
+        help.setMnemonic(KeyEvent.VK_H);
         //--------------
-        JMenuItem Preferences = new JMenuItem("Preferences");
-        JMenuItem Changelog = new JMenuItem();
-        JMenuItem HideInsomnia = new JMenuItem("Changelog");
-        JMenuItem HideOthers = new JMenuItem("HideOthers");
-        JMenuItem Quit = new JMenuItem("Quit");
+        /*
+            menuItems of the application menu
+         */
+        JMenuItem preferences = new JMenuItem("Preferences");
+        preferences.setMnemonic((KeyEvent.VK_P));
+        preferences.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose preferences from the Application of the menubar");
+            }
+        });
+        application.add(preferences);
+
+        JMenuItem changelog = new JMenuItem("Changelog");
+        changelog.setMnemonic(KeyEvent.VK_C);
+        changelog.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose change log from the Application of the menubar");
+            }
+        });
+        application.add(changelog);
+
+        application.addSeparator();
+
+        JMenuItem hideInsomnia = new JMenuItem("Hide Insomnia");
+        hideInsomnia.setMnemonic(KeyEvent.VK_H);
+        hideInsomnia.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose hide insomnia from the Application of the menubar");
+            }
+        });
+        application.add(hideInsomnia);
+
+        JMenuItem hideOthers = new JMenuItem("HideOthers");
+        hideOthers.setMnemonic(KeyEvent.VK_I);
+        hideOthers.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose hide others from the Application of the menubar");
+            }
+        });
+        application.add(hideOthers);
+
+        application.addSeparator();
+
+        JMenuItem quit = new JMenuItem("Quit", new ImageIcon("C:\\Users\\venus\\Desktop\\uni\\barnameneVC pishrafte\\ProjeMid\\src\\GUI\\log-off-icon.png"));
+        quit.setMnemonic(KeyEvent.VK_Q);
+        quit.setToolTipText("quit the application");
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose quit from the Application of the menubar");
+            }
+        });
+        application.add(quit);
+
+        //--------------
+        /*
+            menuItems of the Edit menu
+         */
+        JMenuItem undo = new JMenuItem("Undo", new ImageIcon("C:\\Users\\venus\\Desktop\\uni\\barnameneVC pishrafte\\ProjeMid\\src\\GUI\\undo-icon.png"));
+        undo.setMnemonic((KeyEvent.VK_U));
+        undo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose undo from the Edit of the menubar");
+            }
+        });
+        edit.add(undo);
+
+        JMenuItem redo = new JMenuItem("Cut", new ImageIcon("C:\\Users\\venus\\Desktop\\uni\\barnameneVC pishrafte\\ProjeMid\\src\\GUI\\redo-icon.png"));
+        redo.setMnemonic(KeyEvent.VK_R);
+        redo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose redo from the Edit of the menubar");
+            }
+        });
+        edit.add(redo);
+
+        edit.addSeparator();
+
+        JMenuItem cut = new JMenuItem("Cut", new ImageIcon("C:\\Users\\venus\\Desktop\\uni\\barnameneVC pishrafte\\ProjeMid\\src\\GUI\\cut-icon.png"));
+        cut.setMnemonic(KeyEvent.VK_C);
+        cut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose cut insomnia from the edit menubar");
+            }
+        });
+        edit.add(cut);
+
+        JMenuItem copy = new JMenuItem("Copy", new ImageIcon("C:\\Users\\venus\\Desktop\\uni\\barnameneVC pishrafte\\ProjeMid\\src\\GUI\\copy-icon.png"));
+        copy.setMnemonic(KeyEvent.VK_O);
+        copy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose copy insomnia from the edit menubar");
+            }
+        });
+        edit.add(copy);
+        JMenuItem paste = new JMenuItem("Paste");
+        paste.setMnemonic(KeyEvent.VK_P);
+        paste.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose paste insomnia from the edit menubar");
+            }
+        });
+        edit.add(paste);
+        JMenuItem selectAll = new JMenuItem("Select All");
+        selectAll.setMnemonic(KeyEvent.VK_S);
+        selectAll.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("choose Select All insomnia from the edit menubar");
+            }
+        });
+        edit.add(selectAll);
+
+        /*
+            menuItems of the View menu
+         */
+        JMenuItem toggleFullScreen= new JMenuItem("Toggle Full Screen");
+        //source of the next line:
+        //https://stackoverflow.com/questions/61576224/swing-full-screen-shortcut
+        toggleFullScreen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F11, KeyEvent.SHIFT_MASK));
+        toggleFullScreen.addActionListener(e->mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH));
+        view.add(toggleFullScreen);
+
+        /*
+            menuItems of the Help menu
+         */
+        JMenuItem keyBoardShortCuts= new JMenuItem("KeyBoard Short Cuts");
+        keyBoardShortCuts.setMnemonic(KeyEvent.VK_K);
+        help.add(keyBoardShortCuts);
+
+        JMenuItem about = new JMenuItem("About");
+        about.setMnemonic(KeyEvent.VK_O);
+        help.add(about);
+
+
+
 
     }
 }
